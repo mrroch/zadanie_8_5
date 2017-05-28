aeb1ea19f7e6c4ef519dbcfe6a43f33064d2fa8a
Michał <michalroch@wp.pl>
2017-05-28 10:39:57 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// scripts js
+
+var a = prompt('podaj podstawę trójkąta:');
+var h = prompt('podaj wysokość trójkąta:');
+
+var triangleArea = a * h / 2;
+
+alert('Pole trójkąta wynosi ' + triangleArea);
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
